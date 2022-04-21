51b650e8ec08d8d4642a5879010b243231623c3d
NICOLAE ASADULLIN <72973918+Itachi253@users.noreply.github.com>
2022-04-21 13:27:42 +0300

    update
    up

---
+++ lab-2/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CR-193 Asadullin Nicolae</h1>
+      <h2>UTM</h2>
+    </div>
+  );
+}
+
+export default App;
